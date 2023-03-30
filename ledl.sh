3926c33bbd1f21366c83fb0753769ebff529441f
grey <gregorypro@protonmail.com>
2023-03-30 22:07:48 +0200

    single input variable

--- ledl.sh
+++ ledl.sh
@@ -12,56 +12,54 @@ function newMoviesList() {
 	echo "This is a list of movies you haven't watched yet:"
 	echo ""
 	ls /Volumes/Data/Shared/movies/__new_movies
-	read -p "Which movie do you wish to download?: " ms
+	read -p "Which movie do you wish to download?: " input
 }
 
 function newSeriesList() {
 	echo "This is a list of series you haven't watched yet:"
 	echo ""
 	ls /Volumes/Data/Shared/series/__new_series
-	read -p "Which series do you want to download?: " ss
+	read -p "Which series do you want to download?: " input
 }
 
 function newMovieDownload() {
-	if [ -d ~/Movies/"$ms" ]; then
+	if [ -d ~/Movies/"$input" ]; then
 		echo "Destination directory already exists downloading non-exsiting files..."
-		rsync -avz -r --ignore-existing --progress /Volumes/Data/Shared/movies/__new_movies/*"$ms"* ~/Movies/"$ms"
+		rsync -avz -r --ignore-existing --progress /Volumes/Data/Shared/movies/__new_movies/*"$input"* ~/Movies/"$input"
 	else
 		echo "Creating download directory..."
-		mkdir ~/Movies/"$ms"/
-		rsync -avz -r --progress /Volumes/Data/Shared/movies/__new_movies/*"$ms"* ~/Movies/"$ms"
+		mkdir ~/Movies/"$input"/
+		rsync -avz -r --progress /Volumes/Data/Shared/movies/__new_movies/*"$input"* ~/Movies/"$input"
 	fi
 }
 
 function newSeriesDownload() {
-	if [ -d ~/Movies/"$ss" ]; then
+	if [ -d ~/Movies/"$input" ]; then
 		echo "Destination directory already exists downloading non-exsiting files..."
-		rsync -avz -r --ignore-existing --progress /Volumes/Data/Shared/series/__new_series/*"$ss"* ~/Movies/"$ss"
+		rsync -avz -r --ignore-existing --progress /Volumes/Data/Shared/series/__new_series/*"$input"* ~/Movies/"$input"
 	else
 		echo "Creating download directory..."
-		mkdir ~/Movies/"$ss"/
-		rsync -avz -r --progress /Volumes/Data/Shared/series/__new_series/*"$ss"* ~/Movies/"$ss"
+		mkdir ~/Movies/"$input"/
+		rsync -avz -r --progress /Volumes/Data/Shared/series/__new_series/*"$input"* ~/Movies/"$input"
 	fi
 }
 
-
 if [ -d "/Volumes/Data/Shared" ]; then
 	categoryChoice
-	
 	#Movie SECTION START
 	if [[ "$category" == "1" ]]; then
 		newMoviesList
-		if [[ "$ms" == "q" ]]; then
+		if [[ "$input" == "q" ]]; then
 			echo "Quitting..."
 		else
 			newMovieDownload
 		fi
 	#Movie SECTION END
-
+	
 	#Series SECTION START
 	elif [[ "$category" == "2" ]]; then
 		newSeriesList
-		if [[ "$ss" == "q" ]]; then
+		if [[ "$input" == "q" ]]; then
 			echo "Quitting..."
 		else
 			newSeriesDownload
