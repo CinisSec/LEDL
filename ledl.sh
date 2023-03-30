f5519c132957019f2ee1be81c99dec6f0e0f2aa4
grey <gregorypro@protonmail.com>
2023-03-30 22:20:49 +0200

    formatting

--- ledl.sh
+++ ledl.sh
@@ -1,27 +1,25 @@
 #!/bin/bash
 #LEDL - Local Entertainment Downloader
 
+#FUNCTIONS SECTION START
 function categoryChoice() {
 	echo "Which category do you want to download?"
 	echo "[1] movies"
 	echo "[2] series"
 	read -p "Input choice: [1-2]" category
 }
-
 function newMoviesList() {
 	echo "This is a list of movies you haven't watched yet:"
 	echo ""
 	ls /Volumes/Data/Shared/movies/__new_movies
 	read -p "Which movie do you wish to download?: " input
 }
-
 function newSeriesList() {
 	echo "This is a list of series you haven't watched yet:"
 	echo ""
 	ls /Volumes/Data/Shared/series/__new_series
 	read -p "Which series do you want to download?: " input
 }
-
 function newMovieDownload() {
 	if [ -d ~/Movies/"$input" ]; then
 		echo "Destination directory already exists downloading non-exsiting files..."
@@ -32,7 +30,6 @@ function newMovieDownload() {
 		rsync -avz -r --progress /Volumes/Data/Shared/movies/__new_movies/*"$input"* ~/Movies/"$input"
 	fi
 }
-
 function newSeriesDownload() {
 	if [ -d ~/Movies/"$input" ]; then
 		echo "Destination directory already exists downloading non-exsiting files..."
@@ -43,10 +40,11 @@ function newSeriesDownload() {
 		rsync -avz -r --progress /Volumes/Data/Shared/series/__new_series/*"$input"* ~/Movies/"$input"
 	fi
 }
+#FUNCTIONS SECTION END
 
+#MAIN SECTION START
 if [ -d "/Volumes/Data/Shared" ]; then
 	categoryChoice
-	#Movie SECTION START
 	if [[ "$category" == "1" ]]; then
 		newMoviesList
 		if [[ "$input" == "q" ]]; then
@@ -54,9 +52,6 @@ if [ -d "/Volumes/Data/Shared" ]; then
 		else
 			newMovieDownload
 		fi
-	#Movie SECTION END
-	
-	#Series SECTION START
 	elif [[ "$category" == "2" ]]; then
 		newSeriesList
 		if [[ "$input" == "q" ]]; then
@@ -65,7 +60,7 @@ if [ -d "/Volumes/Data/Shared" ]; then
 			newSeriesDownload
 		fi
 	fi
-	#Series SECTION END
 else
 	echo "Volume is not mounted"
 fi
+#MAIN SECTION END
