4fd1cbd5b3dc0e8406393d64e6e787cc181ad2df
grey <gregorypro@protonmail.com>
2024-05-12 03:04:08 +0200

    Revert "Reorganize ledl2.sh code and try to port to python. rsync with python not working yet."
    This reverts commit 50486a0eadc7ea22c24aad0b4689b33bdebf03cb.

--- ledl2.sh
+++ ledl2.sh
@@ -2,24 +2,9 @@
 #LEDL - Local Entertainment Downloader v2
 #Code can be used freely as long as I am credited
 #Rewrite of ledl.sh to optimize linecount and readeability
-function categoryChoice {
-    echo "Which category do you want to download?"
-    echo "[1] movies"
-    echo "[2] series"
-    echo "[3] anime"
-    read -p "Category choice: [1-3]" category
-    case $category in
-    1) category="movies" ;;
-    2) category="series" ;;
-    3) category="anime" ;;
-    esac
-}
-function oldvnew {
-    read -n1 -p "Old or new entertainment? [o/n] " type
-}
 function list {
     echo "This is a list of" "$category" "that is available:"
-    echo ""
+    echo ""§
     case "$type" in
     o) ls /Volumes/Data/Shared/"$category" ;;
     n) ls /Volumes/Data/Shared/"$category"/__new_"$category" ;;
@@ -35,6 +20,21 @@ function download {
         rsync -avz -r --ignore-existing --progress /Volumes/Data/Shared/"$category"/__new_"$category"/*$title* ~/Movies/"$title"
     else return 0
     fi
+}
+function categoryChoice {
+    echo "Which category do you want to download?"
+    echo "[1] movies"
+    echo "[2] series"
+    echo "[3] anime"
+    read -p "Category choice: [1-3]" category
+    case $category in
+    1) category="movies" ;;
+    2) category="series" ;;
+    3) category="anime" ;;
+    esac
+}
+function oldvnew {
+    read -n1 -p "Old or new entertainment? [o/n] " type
 }
 #MAIN SECTION START
 #Check if volume is mounted and run functions else print error
